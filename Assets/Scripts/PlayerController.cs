2e36587c93b2cf6336a87bea195dc2e9beb13246
zaruen <thomasneuraz@gmail.com>
2016-03-03 23:50:56 +0800

    Implement Collect pickups

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -21,4 +21,11 @@ public class PlayerController : MonoBehaviour
 
         rb.AddForce(movement * speed);
     }
+
+	void OnTriggerEnter(Collider other){
+		//Destroy (other.gameObject);
+		if (other.gameObject.CompareTag ("Pick Up")) {
+			other.gameObject.SetActive (false);
+		}
+	}
 }
