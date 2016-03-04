e6b45bd9ed1794f6522303d2a7e6068567117f81
Thomas Neuraz [HK-ITS] <TNeuraz@kerrylogistics.com>
2016-03-04 18:11:28 +0800

    finish game

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed;
+    public float Speed;
+    public Text WinText;
+    public Text ScoreText;
 
     private Rigidbody rb;
+    private int score;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        score = 0;
+        SetScoreText();
+        WinText.text = "";
     }
 
     void FixedUpdate()
@@ -19,13 +26,25 @@ public class PlayerController : MonoBehaviour
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
-        rb.AddForce(movement * speed);
+        rb.AddForce(movement * Speed);
     }
 
 	void OnTriggerEnter(Collider other){
-		//Destroy (other.gameObject);
 		if (other.gameObject.CompareTag ("Pick Up")) {
 			other.gameObject.SetActive (false);
+            score ++;
+		    SetScoreText();
 		}
 	}
+
+    void SetScoreText()
+    {
+        ScoreText.text = "Score: " + score;
+
+        if (score >= 12)
+        {
+            WinText.text = "YOU WIN !";
+            Speed = 0;
+        }
+    }
 }
